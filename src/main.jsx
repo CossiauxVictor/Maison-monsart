1a594dc0dc25f9dc699e267f250c136aa5d4c845
CossiauxVictor <victor.cossiaux.p@gmail.com>
2026-03-18 14:09:23 +0100

    B2B

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
+import Partenaires from './pages/Partenaires.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/partenaires" element={<Partenaires />} />
+        <Route path="/b2b" element={<Navigate to="/partenaires" replace />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 )
